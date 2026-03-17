91655bda6a6fd013f4b6c343de4b71471c218d67
avinashkonangi-git <avinash.konangi.ai@gmail.com>
2026-03-17 12:58:40 +0530

    File updated

--- indian-address-block.php
+++ indian-address-block.php
@@ -5,7 +5,7 @@
  * Version:           1.0
  * Requires at least: 6.8
  * Requires PHP:      7.4
- * Author:            Avinash
+ * Author:            Konangi Avinash
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       indian-address-block
